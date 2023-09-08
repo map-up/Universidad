6cd9f1b6133009c039cb0f6ca06d4e7277706f2e
map-up <141764918+map-up@users.noreply.github.com>
2023-09-08 16:14:53 -0300

    PackageEntidades/Alumno
    datos

--- src/universidad/entidades/Alumno.java
+++ src/universidad/entidades/Alumno.java
@@ -5,11 +5,78 @@
  */
 package universidad.entidades;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Manuel
  */
 public class Alumno {
 
+    private int idAlumno; 
+    private String apellido, nombre;
+    private LocalDate fechaNac;
+    boolean activo;
 
+    //constructor vacio
+    public Alumno() {
+    }
+    
+    //Consctructor completo
+    public Alumno(int idAlumno, String apellido, String nombre, LocalDate fechaNac) {
+        this.idAlumno = idAlumno;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+    }
+    
+    //Constructor sin idAlumno
+    public Alumno(String apellido, String nombre, LocalDate fechaNac, boolean activo) {
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+        this.activo = activo;
+    }
+    
+    //getter and setter
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(LocalDate fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+    
 }
